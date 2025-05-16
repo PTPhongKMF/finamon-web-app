1cc7a5f79556e5cebf05596204b611498ff5abbb
PTPhong-PPL <PTPhongSWB@gmail.com>
2025-05-17 01:39:56 +0700

    chore: update eslint and add packages

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,11 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
 export default [
   { ignores: ['dist'] },
+  ...pluginQuery.configs['flat/recommended'],
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
