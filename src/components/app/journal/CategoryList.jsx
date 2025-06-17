60a8b05abfe73e218d886dce35f245cf09d4a287
PTPhong-PPL <PTPhongSWB@gmail.com>
2025-06-17 11:52:28 +0700

    feat(app): add edit category

--- src/components/app/journal/CategoryList.jsx
+++ src/components/app/journal/CategoryList.jsx
@@ -1,11 +1,14 @@
 import { useMutation, useQuery } from "@tanstack/react-query"
 import { kyAspDotnet } from "../../../api/ky"
 import { useUserStore } from "../../../stores/userStore"
-import { useEffect, useState } from "react";
-import { EllipsisVertical, Layers, CheckCircle2 } from "lucide-react";
+import { useState } from "react";
+import { EllipsisVertical, Layers, CheckCircle2, SquarePen } from "lucide-react";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../../shadcn/dropdown-menu";
 import { m } from "../../../i18n/paraglide/messages";
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "../../shadcn/alert-dialog";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "../../shadcn/dialog";
+import { Button } from "../../shadcn/button";
+import { Input } from "../../shadcn/input";
 
 export default function CategoryList() {
   const [forcedLoading, setForcedLoading] = useState(false);
@@ -29,10 +32,10 @@ export default function CategoryList() {
   })
 
   const deleteCategory = useMutation({
-    mutationFn: async (id) => {
+    mutationFn: async () => {
       setShowDeleteDialog(false);
       setForcedLoading(true);
-      return await kyAspDotnet.delete(`api/category/${id}`).json();
+      return await kyAspDotnet.delete(`api/category/${selectedCategory.id}`).json();
     },
     onSuccess: () => {
       fetchCategories.refetch();
@@ -42,8 +45,24 @@ export default function CategoryList() {
     onSettled: () => setForcedLoading(false)
   })
 
-  useEffect(() => {
-    console.log(fetchCategories?.data?.items);
+  const editCategory = useMutation({
+    mutationFn: async () => {
+      setShowEditDialog(false);
+      setForcedLoading(true);
+      return await kyAspDotnet.put(`api/category/${selectedCategory.id}`, {
+        json: {
+          name: selectedCategory.name,
+          color: selectedCategory.color,
+          userId: user.id
+        }
+      }).json();
+    },
+    onSuccess: () => {
+      fetchCategories.refetch();
+      setShowSuccess(true);
+      setTimeout(() => setShowSuccess(false), 1500);
+    },
+    onSettled: () => setForcedLoading(false)
   })
 
   if (fetchCategories.isPending || forcedLoading)
@@ -63,9 +82,13 @@ export default function CategoryList() {
       <>
         <div className="flex gap-2 items-center mb-6">
           <Layers />
+          {editCategory.isError && (
+            <p className="text-red-500 font-semibold max-w-45 break-words">{editCategory.error.message}</p>
+          )}
           {deleteCategory.isError && (
             <p className="text-red-500 font-semibold max-w-45 break-words">{deleteCategory.error.message}</p>
           )}
+
           {showSuccess && (
             <div className="transition-opacity duration-300 opacity-100 text-green-500 flex gap-2 items-center">
               <CheckCircle2 className="w-6 h-6" />
@@ -91,7 +114,11 @@ export default function CategoryList() {
                   </button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end" className="min-w-20">
-                  <DropdownMenuItem>
+                  <DropdownMenuItem
+                    onClick={() => {
+                      setSelectedCategory({ ...category, oldName: category.name });
+                      setShowEditDialog(true)
+                    }}>
                     {m["common.edit"]()}
                   </DropdownMenuItem>
                   <DropdownMenuItem className="text-red-500"
@@ -109,6 +136,35 @@ export default function CategoryList() {
           ))}
         </section>
 
+        <Dialog open={showEditDialog} onOpenChange={setShowEditDialog}>
+          <DialogContent className="p-4">
+            <DialogHeader>
+              <DialogTitle className="flex gap-2 items-center">
+                <SquarePen />
+                <p>{m["app.editCategory"]()} "{selectedCategory.oldName}"</p>
+              </DialogTitle>
+            </DialogHeader>
+            <section className="grid grid-cols-[auto_auto] w-fit grid-rows-2 gap-4 px-4 mt-4 items-center">
+              <label htmlFor="cateName">{m["common.name"]()}</label>
+              <Input type="text" id="cateName" placeholder={m["common.name"]()}
+                value={selectedCategory.name} onChange={(e) => setSelectedCategory({ ...selectedCategory, name: e.target.value })} />
+
+              <label htmlFor="editColor">{m["common.color"]()}</label>
+              <Input id="editColor" type="color" value={selectedCategory.color || "#000000"} onChange={(e) => setSelectedCategory({ ...selectedCategory, color: e.target.value })}
+              />
+            </section>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button className="cursor-pointer" variant="outline">{m["common.cancel"]()}</Button>
+              </DialogClose>
+              <Button className="bg-blue-500 hover:bg-blue-600 cursor-pointer"
+                onClick={editCategory.mutate}>
+                {m["common.confirm"]()}
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+
         <AlertDialog open={showDeleteDialog} onOpenChange={setShowDeleteDialog}>
           <AlertDialogContent>
             <AlertDialogHeader>
@@ -122,7 +178,7 @@ export default function CategoryList() {
             <AlertDialogFooter>
               <AlertDialogCancel className="cursor-pointer">{m["common.cancel"]()}</AlertDialogCancel>
               <AlertDialogAction className="bg-red-500 hover:bg-red-600 cursor-pointer"
-                onClick={() => deleteCategory.mutate(selectedCategory.id)}>
+                onClick={deleteCategory.mutate}>
                 {m["common.confirm"]()}
               </AlertDialogAction>
             </AlertDialogFooter>
